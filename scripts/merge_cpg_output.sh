5d43ea21bac131364a373cf16783b41170bcbd70
Toby Clark <tc1125@login-b.cx3.hpc.ic.ac.uk>
2026-03-24 10:26:56 +0000

    updated shell script

--- scripts/merge_cpg_output.sh
+++ scripts/merge_cpg_output.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #PBS -l select=1:ncpus=65:mem=250gb
 #PBS -l walltime=48:00:00
-#PBS -N run_finemap_local_idx
+#PBS -N merge_susie
+
+cd "$PBS_O_WORKDIR"
 
 eval "$(~/miniforge3/bin/conda shell.bash hook)"
 conda activate finemapping
 
-python merge_cpg_output.py
+python merge_cpg_output.py \
+     --input-dir ../data/susie_results \
+     --output-path ../data/susie_results_merged.csv
