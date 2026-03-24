64bb2f5f7a98c863a4fb0d96904e25253fd06c2e
Toby Clark <tobyclark02@outlook.com>
2026-03-24 10:25:46 +0000

    added functionality to save credible sets

--- scripts/finemap_cpg.R
+++ scripts/finemap_cpg.R
@@ -21,8 +21,24 @@ fitted_z = susie_rss(z = df$Z, R = R, n = N, L = 8)
 
 df$pip <- fitted_z$pip
 
-# get posterior mean effect size
+# posterior mean effect size
 post_mean_beta <- colSums(fitted_z$alpha * fitted_z$mu)
 df$post_mean_beta <- post_mean_beta
+
+# posterior SD of beta
+df$post_sd_beta <- sqrt(pmax(colSums(fitted_z$alpha * fitted_z$mu2) - post_mean_beta^2, 0))
+
+# credible set IDs
+df$cs_id <- NA_character_
+if (!is.null(fitted_z$sets$cs)) {
+  for (i in seq_along(fitted_z$sets$cs)) {
+    idx <- fitted_z$sets$cs[[i]]
+    df$cs_id[idx] <- ifelse(
+      is.na(df$cs_id[idx]),
+      paste0("CS", i),
+      paste(df$cs_id[idx], paste0("CS", i), sep = ";")
+    )
+  }
+}
 
 write.csv(df, paste0(args$out_dir, cpg, '_susie.csv'), row.names = FALSE)
